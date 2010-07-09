516d0c732a248650e2a33c234c9f39dee987079e
Fernando Padilla <fern@alum.mit.edu>
2010-07-09 12:50:45 -0700

    adding cdn module def example

--- examples/yuixdr/module_conf.js
+++ examples/yuixdr/module_conf.js
@@ -21,6 +21,26 @@ var FB_MODULES = {
 	}
 };
 
+var FB_MODULES_CDN = {
+	loadOptional : true,
+	groups : {
+		"gallery-fb" : {
+			combine : false,
+			base : "http://l.yimg.com/a/lib/sp/fn/",
+			modules : {
+				"gallery-fb-connect" : {
+					path : "js/gallery_fb_connect.js?",
+					requires : [ "gallery-fb-connect-css" ]
+				},
+				"gallery-fb-connect-css" : {
+					path : "css/gallery_fb_connect.css?",
+					type : "css"
+				}
+			}
+		}
+	}
+};
+
 var FB_INIT = {
 	FB : {
 		init : {
